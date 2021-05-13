7fe8cc803dc726c0f764d5622379141fc7ff1276
UalissonRodrigues <ualissonfreitas@hotmail.com>
2021-05-13 12:07:09 -0300

    Atualizações e Melhorias

--- index.php
+++ index.php
@@ -33,49 +33,49 @@ include "./include/header.php"
             </div>
         </div>
     </div>
-</header>
-<div class="container_corpo">
-    <div class="row">
-        <div class="col-lg-2">
-            <div style="width: 134%; height: 80px; background: rgb(27 27 27);"></div>
-        </div>
-        <div class="col-lg-1">
-            <img class="menu-hamburgue" src="img/menu-hamburguer.svg" />
-            <span class="span"> Todos os Departamentos </span>
-        </div>
-        <div class="col-lg-1">
-            <img class="img-cinza" src="img/triangulo-cinza.svg" />
-        </div>
-        <div class="col-lg-1">
-            <img class="som" src="img/som-e-video-cinza.svg" />
-            <span class="span2"> Som e Video </span>
-        </div>
-        <div class="col-lg-1">
-            <img class="farol" src="img/farois-lanternas-cinza.svg" />
-            <span class="span2">Faróris e Iluminação</span>
-        </div>
-        <div class="col-lg-1">
-            <img class="acess-int" src="img/acessorios-internos-cinza.svg" />
-            <span class="span2"> Acessórios internos </span>
-        </div>
-        <div class="col-lg-1">
-            <img class="acess-ex" src="img/acessorios-externos-cinza.svg" />
-            <span class="span2"> Acessórios Externos </span>
-        </div>
-        <div class="col-lg-1">
-            <img class="Vest" src="img/vestuarios-cinza.svg" />
-            <span class="span2"> Vestuario </span>
-        </div>
-        <div class="col-lg-1">
-            <img class="infantil" src="img/infantil-cinza.svg" />
-            <span class="span2"> Infantil</span>
-        </div>
-        <div class="col-lg-2">
-            <div class="fim"></div>
-            <img class="carrinho-compra" src="img/carrinho.svg" />
+    <div class="container_corpo">
+        <div class="row">
+            <div class="col-lg-2">
+                <div style="width: 134%; height: 80px; background: rgb(27 27 27);"></div>
+            </div>
+            <div class="col-lg-1">
+                <img class="menu-hamburgue" src="img/menu-hamburguer.svg" />
+                <span class="span"> Todos os Departamentos </span>
+            </div>
+            <div class="col-lg-1">
+                <img class="img-cinza" src="img/triangulo-cinza.svg" />
+            </div>
+            <div class="col-lg-1">
+                <img class="som" src="img/som-e-video-cinza.svg" />
+                <span class="span2"> Som e Video </span>
+            </div>
+            <div class="col-lg-1">
+                <img class="farol" src="img/farois-lanternas-cinza.svg" />
+                <span class="span2">Faróris e Iluminação</span>
+            </div>
+            <div class="col-lg-1">
+                <img class="acess-int" src="img/acessorios-internos-cinza.svg" />
+                <span class="span2"> Acessórios internos </span>
+            </div>
+            <div class="col-lg-1">
+                <img class="acess-ex" src="img/acessorios-externos-cinza.svg" />
+                <span class="span2"> Acessórios Externos </span>
+            </div>
+            <div class="col-lg-1">
+                <img class="Vest" src="img/vestuarios-cinza.svg" />
+                <span class="span2"> Vestuario </span>
+            </div>
+            <div class="col-lg-1">
+                <img class="infantil" src="img/infantil-cinza.svg" />
+                <span class="span2"> Infantil</span>
+            </div>
+            <div class="col-lg-2">
+                <div class="fim"></div>
+                <img class="carrinho-compra" src="img/carrinho.svg" />
+            </div>
         </div>
     </div>
-</div>
+</header>
 <div class="container_carrosel">
     <div class="row">
         <div class="col-lg-10">
@@ -86,33 +86,82 @@ include "./include/header.php"
         <img class="branco2" src="img/triangulo-branco2.svg" />
     </div>
 </div>
-<div class="container_banner"></div>
-<div class="row banner-row">
-    <div class="col-lg-3 Entrega">
-        <img class="" src="img/entrega.svg" />
-        <h5>Entregamos para todo o pais </h5>
+<div class="row icon">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-2 nopadding">
+        <div class="card Entrega">
+            <div class="card-body">
+                <img class="ent" src="img/entrega.svg" />
+                <h6 class="card-title">Entregamos para todo o pais</h6>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-3 cartao">
-        <img class="" src="img/parcelamento.svg" />
-        <h5>Parcelamos nos Cartão </h5>
+    <div class="col-sm-2 nopadding">
+        <div class="card cartao ">
+            <div class="card-body">
+                <img class="par" src="img/parcelamento.svg" />
+                <h6 class="card-title">Parcelamos nos Cartão </h6>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-3 seguranca">
-        <img class="" src="img/seguranca.svg" />
-        <h5>Site seguro e confíavel </h5>
+    <div class="col-sm-2 nopadding">
+        <div class="card seguranca ">
+            <div class="card-body">
+                <img class="seg" src="img/seguranca.svg" />
+                <h6 class="card-title">Site seguro e confíavel </h6>
+            </div>
+        </div>
     </div>
-    <div class="col-lg-3 troca">
-        <img class="" src="img/troca.svg" />
-        <h5>Primeira troca Gratis </h5>
+    <div class="col-sm-2 nopadding">
+        <div class="card troca ">
+            <div class="card-body">
+                <img class="tro" src="img/troca.svg" />
+                <h6 class="card-title">Primeira troca Gratis</h6>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-2"></div>
+</div>
+<div class="row banner">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-4 ">
+        <div class="card bannervermelho">
+            <div class="card-body-mini">
+                <img class="mini" src="img/mini-banner-esq.jpg" />
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4 ">
+        <div class="card bannerpreto ">
+            <div class="card-body-mini">
+                <img class="mini" src="img/mini-banner-dir.jpg" />
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-2 "></div>
+</div>
+<div class="row vendidos ">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-1 nopadding">
+        <div class="card">
+            <div class="card-body-vend">
+                <img class="enfeite1" src="img/enfeite-mais-vendidos-esq.svg" />
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-8 nopadding">
+        <div class="card">
+            <h4 class="card-title-vend"><b>Mais Vendidos</b></h4>
+        </div>
+    </div>
+    <div class="col-sm-1 nopadding">
+        <div class="card">
+            <div class="card-body-vend">
+                <img class="enfeite2" src="img/enfeite-mais-vendidos-dir.svg" />
+            </div>
+        </div>
     </div>
 </div>
-
-
-
-
-
-
-
-
 <?php
 include "./include/footer.php"
 ?>
