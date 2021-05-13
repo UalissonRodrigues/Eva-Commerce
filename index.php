094d864005d44d7668943b6906fb493c66240e1c
UalissonRodrigues <ualissonfreitas@hotmail.com>
2021-05-12 22:28:59 -0300

    Ajustes e melhorias no vizual

--- index.php
+++ index.php
@@ -1,52 +1,34 @@
 <?php
 include "./include/header.php"
 ?>
-
 <header class="header">
     <div class="row">
         <div class="col-lg-12">
             <div style="width: 100%; height: 50px; background: #ffff;"></div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-3">
-            <div class="logo-container">
-                <a href="" class="navbar-brand">
-                    <img class="img-fluid img_logo" src="img/logo-header.svg" alt="Logo" /></a>
-            </div>
+    <div class="row container-row">
+        <div class="col-lg-4">
+            <div class="logo-container"><img class="img-fluid img_logo" src="img/logo-header.svg" alt="Logo" /></div>
         </div>
-
-        <div class="col-lg-9 - base_triangulo">
-            <img class="img_triangulo_branco" src="img/triangulo-branco.svg" alt="Lupa" />
-            <img class="img_triangulo_preto" src="img/triangulo-cinza.svg" alt="Lupa" />
-            <div class="nav_menu_topo">
-                <div class="row">
-
-                    <div class="col-lg-1"></div>
-
-                    <div class="col-lg-6">
-                        <div class="content_pesquisa">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Digite aqui o que voce procura.." aria-label="Search">
-                            <img class="img_pesquisa" src="img/botao-lupa.svg" alt="Lupa" />
-                        </div>
-                    </div>
-
-                    <div class="col-lg-2">
-                        <div class="content_atendimento">
-                            <img class="img_atendimento" src="img/atendimento.svg" alt="Lupa" />
-                            <a class="nav-link dropdown-toggle" href="#" id="atendimento" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Atendimento
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-2">
-                        <div class="content_minhaConta">
-                            <img class="img_minhaConta" src="img/minha-conta.svg" alt="Lupa" />
-                            <a class="nav-link dropdown-toggle" href="#" id="minha_conta" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Minha conta
-                            </a>
-                        </div>
-                    </div>
+        <div class="col-lg-2">
+            <img class="img_triangulo_branco" src="img/triangulo-branco.svg" />
+        </div>
+        <div class="col-lg-6">
+            <div class="content_pesquisa">
+                <input class="form-control mr-sm-2" type="search" placeholder="           Digite aqui o que voce procura.." aria-label="Search">
+                <img class="img_pesquisa" src="img/botao-lupa.svg" alt="Lupa" />
+                <div class="content_atendimento">
+                    <img class="img_atendimento" src="img/atendimento.svg" alt="Lupa" />
+                    <a class="nav-link dropdown-toggle" href="#" id="atendimento" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Atendimento
+                    </a>
+                </div>
+                <div class="content_minhaConta">
+                    <img class="img_minhaConta" src="img/minha-conta.svg" alt="Lupa" />
+                    <a class="nav-link dropdown-toggle" href="#" id="minha_conta" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Minha conta
+                    </a>
                 </div>
             </div>
         </div>
@@ -91,11 +73,9 @@ include "./include/header.php"
         <div class="col-lg-2">
             <div class="fim"></div>
             <img class="carrinho-compra" src="img/carrinho.svg" />
-            <div class="fim"></div>
         </div>
     </div>
 </div>
-
 <div class="container_carrosel">
     <div class="row">
         <div class="col-lg-10">
@@ -106,6 +86,26 @@ include "./include/header.php"
         <img class="branco2" src="img/triangulo-branco2.svg" />
     </div>
 </div>
+<div class="container_banner"></div>
+<div class="row banner-row">
+    <div class="col-lg-3 Entrega">
+        <img class="" src="img/entrega.svg" />
+        <h5>Entregamos para todo o pais </h5>
+    </div>
+    <div class="col-lg-3 cartao">
+        <img class="" src="img/parcelamento.svg" />
+        <h5>Parcelamos nos Cartão </h5>
+    </div>
+    <div class="col-lg-3 seguranca">
+        <img class="" src="img/seguranca.svg" />
+        <h5>Site seguro e confíavel </h5>
+    </div>
+    <div class="col-lg-3 troca">
+        <img class="" src="img/troca.svg" />
+        <h5>Primeira troca Gratis </h5>
+    </div>
+</div>
+
 
 
 
